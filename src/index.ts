c55dd2dc4406dc75aeb27d4c107884b45d862f3a
yaroslavdrapei <yarikdrapey@gmail.com>
2025-02-25 12:44:28 +0200

    feat: add user to list of subscribers

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import TelegramBot from 'node-telegram-bot-api';
+import TelegramBot, { Message } from 'node-telegram-bot-api';
 import { Commands } from './commands';
 
 const token = process.env.BOT_TOKEN!;
 const commands = Commands.getCommands();
 
 const bot = new TelegramBot(token, { polling: true });
+
+const subscribers: number[] = [];
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
@@ -17,11 +19,24 @@ bot.onText(/\/start/, (msg) => {
 bot.onText(/\/subscribe/, (msg) => {
   const chatId = msg.chat.id;
 
-  bot.sendMessage(chatId, commands.subscribe);
+  if (!subscribers.includes(chatId)) {
+    subscribers.push(chatId);
+    bot.sendMessage(chatId, commands.subscribe);
+    return;
+  }
+
+  bot.sendMessage(chatId, 'You are already subscribed');
 });
 
 bot.onText(/\/unsubscribe/, (msg) => {
   const chatId = msg.chat.id;
 
-  bot.sendMessage(chatId, commands.unsubscribe);
+  const index = subscribers.indexOf(chatId);
+  if (index > -1) {
+    subscribers.splice(index, 1);
+    bot.sendMessage(chatId, commands.unsubscribe);
+    return;
+  }
+
+  bot.sendMessage(chatId, 'You are already unsubscribed');
 });
