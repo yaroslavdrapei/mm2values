43d031ce882e999e7d82b4c32b8fd5037be5ab3b
yaroslavdrapei <yarikdrapey@gmail.com>
2025-05-09 17:40:19 +0300

    feat(bot): prettify inventory

--- bot/src/commands/inventory.ts
+++ bot/src/commands/inventory.ts
@@ -2,6 +2,7 @@ import TelegramBot, { Message } from 'node-telegram-bot-api';
 import { createOrGetUserInventory } from '../createOrGetUserInventory';
 import { IMarkdown, InventoryItem } from '.././types';
 import { capitalize } from '.././utils';
+import { CATEGORIES } from '../constants';
 
 export const inventoryCommand = async (bot: TelegramBot, msg: Message, markdown: IMarkdown): Promise<void> => {
   const chatId = msg.chat.id;
@@ -12,7 +13,27 @@ export const inventoryCommand = async (bot: TelegramBot, msg: Message, markdown:
     return;
   }
 
+  const messageArr: string[] = [];
+  messageArr.push(markdown.bold('Your inventory:'));
+  messageArr.push(`Current value: ${markdown.bold(inventory.currentValue.toString())}`);
+
   const items: InventoryItem[] = inventory.items;
+
+  CATEGORIES.forEach((category) => {
+    const itemsFromCategory = items.filter((item) => item.item.category == category);
+    itemsToText(category, markdown, itemsFromCategory, messageArr);
+  });
+
+  const itemsNoCategory = items.filter(({ item }) => item.category === '' || item.category === undefined);
+  itemsToText('no category', markdown, itemsNoCategory, messageArr);
+
+  bot.sendMessage(chatId, messageArr.join('\n'), { parse_mode: markdown.type });
+};
+
+const itemsToText = (category: string, markdown: IMarkdown, items: InventoryItem[], messageArr: string[]): void => {
+  if (items.length == 0) return;
+  messageArr.push(markdown.bold(`\n----------- ${capitalize(category)} -----------`));
+
   items.sort((a, b) => {
     const valueA = parseInt(a.item.value.replace(',', ''));
     const valueB = parseInt(b.item.value.replace(',', ''));
@@ -23,16 +44,10 @@ export const inventoryCommand = async (bot: TelegramBot, msg: Message, markdown:
     return valueB - valueA;
   });
 
-  const messageArr: string[] = [];
-  messageArr.push(markdown.bold('Your inventory:'));
-  messageArr.push(`Current value: ${markdown.bold(inventory.currentValue.toString())}`);
-  messageArr.push(markdown.bold('\nItems:'));
   items.forEach(({ item, quantity }) => {
     const text = `${markdown.bold(item.name)} (${quantity}): ${capitalize(item.type)}, ${item.value}, ${
       item.stability
     }`;
     messageArr.push(text);
   });
-
-  bot.sendMessage(chatId, messageArr.join('\n'), { parse_mode: markdown.type });
 };
---
+++ bot/src/constants.ts
@@ -0,0 +1,2 @@
+// Order matters for displaying inventory
+export const CATEGORIES = ['season 1', 'classic', 'christmas', 'halloween', 'pets', 'sets'];
