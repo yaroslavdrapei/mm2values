8d7d1563e3042c8ce0e5c505de8ffd32cf9b6442
yaroslavdrapei <yarikdrapey@gmail.com>
2025-05-09 22:36:33 +0300

    feat(bot): change sorting for inventory; show total value of items

--- bot/src/constants.ts
+++ bot/src/constants.ts
@@ -1,2 +1,16 @@
 // Order matters for displaying inventory
 export const CATEGORIES = ['season 1', 'classic', 'christmas', 'halloween', 'pets', 'sets'];
+export const ITEM_TYPES = [
+  'uniques',
+  'ancients',
+  'godlies',
+  'legendaries',
+  'rares',
+  'uncommons',
+  'commons',
+  'misc',
+  'pets',
+  'vintages',
+  'chromas',
+  'sets'
+];
